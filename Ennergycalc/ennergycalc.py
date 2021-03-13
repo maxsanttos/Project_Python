b9377dba7fd20d73d0130920c698358db1428b1d
MAX SUEL SANTOS BATISTA <64488195+maxsanttos@users.noreply.github.com>
2021-03-13 14:51:07 -0300

    Colocando comentário no código
    Comentário no código

--- Ennergycalc/ennergycalc.py
+++ Ennergycalc/ennergycalc.py
@@ -25,12 +25,12 @@ def calc():
     c = latual - lanterior
     print(f"Seu comsumo é de {c} quilowatss")
 
-def gree(atual):
+def gree(atual): #Função para o calculo da bandeira verde
     p = atual * 0.51
     print(f"Parabéns não haverá nenhum acrescimo no valor da sua fatura, Você irá apenas pagar R$ {p:.2f} reias!")
 
 
-def yellow(atual):
+def yellow(atual): #Função para o calculo da bandeira amarela
     x = atual / 100
     x *= 1.35
     fatura = (atual * 0.51) + x
@@ -38,13 +38,14 @@ def yellow(atual):
     print(f"Sua fatura virá com o valor de R$ {fatura:.2f} reias")
  
 
-def red1(atual):
+def red1(atual): #Função para o calculo da bandeira vermelha patamar 01
         d = atual /100
         d *= 4.17
         fatura = (atual * 0.51) + d
         print(f"Seu Consumo é {atual} Quilowatss,Sua fatura terá um acrescimo de R$ {d:.2f} reias")
         print(f"Sua fatura virá com o valor de R$ {fatura:.2f} reias")
-def red2(atual):
+
+def red2(atual): #Função para o calculo da bandeira Vermelha patamar 02
     t = atual /100
     t *= 6.24
     fatura = (atual * 0.51) + t
